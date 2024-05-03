04d214493ca7d2a54f243d85962705c70d46f8cb
Siri4010374 <S4010374@student.rmit.edu.au>
2024-05-03 21:04:11 +1000

    a

--- Activity4.java
+++ Activity4.java
@@ -1,3 +1,51 @@
-public class Activity4 {
+import java.util.List;
+class User{
+
+}
+class guestUser {
+    private String guestID;
+
+    public void browseQuestion{
+        return 
+    }
+    public viewQuestionDetail();
+
+
     
 }
+class registeredUser {
+    private String userID;
+    private String registeredDate;
+    private String subscriptionType; 
+
+    public logIn();
+    public viewQuestionDetail();
+    public askQuestion();
+    public answerQuestion();
+    public editQuestion();
+    public editAnswer();
+    public buyPremiumTier();
+    
+}
+class Subscription {
+    private int subscibtionId;
+    private String subscriptionEndDate;
+    private String subscriptionType;
+
+    public void UpdatePremium();
+    
+}
+class Payment {
+    private Int paymentId;
+    private Double paymentAmount; 
+
+    public logIn();
+    public viewQuestionDetail();
+}
+class Main {
+    
+}
+class Main {
+    
+}
+
